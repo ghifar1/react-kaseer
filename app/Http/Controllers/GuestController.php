3553e1f875cfe0f36134f39998bbb12805239240
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-10-18 20:57:26 +0700

    login redirect

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class GuestController extends Controller
 {
     public function index()
     {
+        if(Auth::check())
+        {
+            return redirect('/home');
+        }
         return Inertia::render('Login');
     }
 }
